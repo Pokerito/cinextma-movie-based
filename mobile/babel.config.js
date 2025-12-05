2f0fbbff6dd0d9ee0cc13b5b39b3c8d29026035d
Pokerito <gaurav704243@gmail.com>
2025-12-06 02:20:27 +0530

    fix: Update mobile env config and dependencies for Android build

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -5,5 +5,20 @@ module.exports = function (api) {
             ["babel-preset-expo", { jsxImportSource: "nativewind" }],
             "nativewind/babel",
         ],
+        plugins: [
+            [
+                "transform-inline-environment-variables",
+                {
+                    include: [
+                        "NEXT_PUBLIC_TMDB_ACCESS_TOKEN",
+                        "NEXT_PUBLIC_SUPABASE_URL",
+                        "NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY",
+                        "NEXT_PUBLIC_CAPTCHA_SITE_KEY",
+                        "NEXT_PUBLIC_AVATAR_PROVIDER_URL"
+                    ],
+                },
+            ],
+            ["module:react-native-dotenv"]
+        ],
     };
 };
